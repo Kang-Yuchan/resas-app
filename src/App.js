a2aa80e18e90c43672038cdb9c91d23e99429aff
Yuchan <mikuplayer123@gmail.com>
2020-05-21 07:03:54 +0900

    change to class component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { Component } from "react";
 import styled from "styled-components";
-import { apiData } from "./api";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+import { apiKey } from "./api";
 
 const Title = styled.div`
   background-color: #99e2ef;
@@ -12,15 +14,24 @@ const Title = styled.div`
   padding: 20px 0;
 `;
 
-const Container = styled.div`
-  margin: 0 auto;
+const Main = styled.div`
   max-width: 1200px;
   display: flex;
   flex-direction: column;
-  color: white;
+  margin: 0 auto;
+`;
+
+const Container = styled.div`
+  margin: 20px auto;
+  display: grid;
+  max-width: 1200px;
+  min-height: 300px;
+  margin-bottom: 80px;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
 `;
 
 const Label = styled.div`
+  position: relative;
   border: 1px solid #99e2ef;
   background-color: #99e2ef;
   display: flex;
@@ -29,20 +40,121 @@ const Label = styled.div`
   border-radius: 5px;
   width: 150px;
   height: 50px;
-  margin: 30px; 0 0 20px;
+  margin: 30px 0 30px 168px;
 `;
 
-const App = () => {
-  return (
-    <div className="App">
-      <Title>
-        <h1>RESAS APP</h1>
-      </Title>
-      <Container>
-        <Label>都道府県</Label>
-      </Container>
-    </div>
-  );
-};
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      selected: Array(47).fill(false),
+      prefectures: {},
+      series: [],
+    };
+    this._changeSelection = this._changeSelection.bind(this);
+  }
+
+  componentDidMount() {
+    fetch("https://opendata.resas-portal.go.jp/api/v1/prefectures", {
+      headers: { "X-API-KEY": apiKey },
+    })
+      .then((response) => response.json())
+      .then((res) => {
+        this.setState({ prefectures: res.result });
+      });
+  }
+
+  _changeSelection(index) {
+    const selected_copy = this.state.selected.slice();
+    selected_copy[index] = !selected_copy[index];
+
+    if (!this.state.selected[index]) {
+      fetch(
+        `https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=-&prefCode=${
+          index + 1
+        }`,
+        {
+          headers: { "X-API-KEY": apiKey },
+        }
+      )
+        .then((response) => response.json())
+        .then((res) => {
+          let tmp = [];
+          Object.keys(res.result.data).forEach((i) => {
+            tmp.push(res.result.data[i].data[0].value);
+          });
+          const res_series = {
+            name: this.state.prefectures[index].prefName,
+            data: tmp,
+          };
+          this.setState({
+            selected: selected_copy,
+            series: [...this.state.series, res_series],
+          });
+        });
+    } else {
+      const series_copy = this.state.series.slice();
+
+      for (let i = 0; i < series_copy.length; i++) {
+        if (series_copy[i].name == this.state.prefectures[index].prefName) {
+          series_copy.splice(i, 1);
+        }
+      }
+      this.setState({
+        selected: selected_copy,
+        series: series_copy,
+      });
+    }
+  }
+
+  renderItem(props) {
+    return (
+      <div key={props.prefCode} style={{ margin: "5px", width: "100px" }}>
+        <input
+          type="checkbox"
+          checked={this.state.selected[props.prefCode - 1]}
+          onChange={() => this._changeSelection(props.prefCode - 1)}
+        />
+        <span>{props.prefName}</span>
+      </div>
+    );
+  }
+
+  render() {
+    const obj = this.state.prefectures;
+    const options = {
+      chart: {
+        type: "line",
+      },
+      title: {
+        text: "総合人口推移",
+      },
+      plotOptions: {
+        series: {
+          label: {
+            connectorAllowed: false,
+          },
+          pointInterval: 10,
+          pointStart: 1970,
+        },
+      },
+      series: this.state.series,
+    };
+    return (
+      <>
+        <div>
+          <Title>RESAS APP</Title>
+          <Main>
+            <Label>都道府県</Label>
+            <Container>
+              {Object.keys(obj).map((i) => this.renderItem(obj[i]))}
+            </Container>
+            <HighchartsReact highcharts={Highcharts} options={options} />
+          </Main>
+        </div>
+      </>
+    );
+  }
+}
 
 export default App;
--- src/api.js
+++ src/api.js
@@ -1,16 +1,1 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "https://opendata.resas-portal.go.jp/",
-  headers: {
-    "X-API-KEY": "Zp0ThlAW2efTLOAfvaiXdOBg6fT2XAPw7EAulBrX",
-  },
-});
-
-export const apiData = {
-  location: () => api.get("api/v1/prefectures"),
-  population: (code) =>
-    api.get(
-      `api/v1/population/composition/perYear?cityCode=-&prefCode=${code}`
-    ),
-};
+export const apiKey = "s3sasmHfgS971K8ZAQudZZuYpdrx5pVMCBnmW1tR";
